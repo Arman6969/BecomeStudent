6dedd078640ed119445cd58436538594b5da6899
Arman6969 <117938877+Arman6969@users.noreply.github.com>
2022-11-20 11:52:11 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let number1 = 69;
+let number2 = 9;
+if (number1 > number2) {
+    console.log(number1 + number2);
+    console.log(number1 - number2);
+    console.log(number1 * number2);
+    console.log(number1 / number2);
+    console.log(number1 % number2);
+} else {
+    console.log("number1 < number2");
+}
+
+console.log('My name is Arman, I am 18');
+
+let number3 = -96;
+if (number3 < 0) {
+    console.log('yes');
+} else {
+    console.log('no');
+} 
+
+let number4 = 669;
+if (number4 % 2 == 0) {
+    console.log('even');
+} else {
+    console.log('odd');
+} 
+
+let number5 = 996;
+let number6 = 669;
+if (number5 % number6 == 0 || number6 % number5 == 0) {
+    console.log('1');
+} else {
+    console.log('0');
+} 
+
